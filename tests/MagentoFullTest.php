65fef5b8af761264674182c6305cba2e270b75c8
Calara Ionut <ionut.calara@gmail.com>
2019-06-12 12:43:20 +0300

    Update version, change module to work with automated tests

---
+++ tests/MagentoFullTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Magento;
+
+
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Facebook\WebDriver\Exception\TimeOutException;
+use Facebook\WebDriver\Exception\UnexpectedTagNameException;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Lmc\Steward\Test\AbstractTestCase;
+
+/**
+ * @group magento_full_test
+ */
+class MagentoFullTest extends AbstractTestCase {
+
+	public $runner;
+
+	/**
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testGeneralFunctions() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'settings_check' => true,
+			)
+		);
+	}
+
+
+	/**
+	 *
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testUsdPaymentBeforeOrderInstant() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'first_test'             => true,
+				'currency'               => 'USD',
+				'stop_emails'            => true,
+				'capture_mode'           => 'instant',
+				'checkout_mode'          => 'before_order',
+			)
+		);
+	}
+
+	/**
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testUsdPaymentBeforeOrderDelayed() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'capture_mode'           => 'delayed',
+				'checkout_mode'          => 'before_order',
+			)
+		);
+	}
+
+
+	/**
+	 *
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testEURPaymentBeforeOrderInstant() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'currency'               => 'EUR',
+				'capture_mode'           => 'instant',
+				'checkout_mode'          => 'before_order',
+			)
+		);
+	}
+
+	/**
+	 *
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testEURPaymentBeforeOrderDelayed() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'currency'               => 'EUR',
+				'capture_mode'           => 'delayed',
+				'checkout_mode'          => 'before_order',
+			)
+		);
+	}
+
+	/**
+	 *
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testDkkPaymentBeforeOrderInstant() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'currency'               => 'DKK',
+				'capture_mode'           => 'instant',
+				'checkout_mode'          => 'before_order',
+				'exclude_manual_payment' => false,
+			)
+		);
+	}
+
+
+	/**
+	 * @throws NoSuchElementException
+	 * @throws TimeOutException
+	 * @throws UnexpectedTagNameException
+	 */
+	public function testJpyPaymentBeforeOrderDelayed() {
+		$this->runner = new MagentoRunner( $this );
+		$this->runner->ready( array(
+				'currency'               => 'JPY',
+				'capture_mode'           => 'delayed',
+				'checkout_mode'          => 'before_order',
+				'stop_emails'            => true,
+			)
+		);
+	}
+
+}
