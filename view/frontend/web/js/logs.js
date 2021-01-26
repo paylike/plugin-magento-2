6703642e98197b76f6934676160900803dd6806a
Ionut Calara <ionut.calara@gmail.com>
2021-01-26 10:18:37 +0200

    Increment version

---
+++ view/frontend/web/js/logs.js
@@ -0,0 +1,37 @@
+window.PaylikeLogger = {
+    date: new Date().getTime(),
+    context: {},
+    setContext: function(context, $, url) {
+      if (!this.enabled()) {
+        console.log("logs not enabled, stopping");
+        return;
+      }
+
+      this.context = context;
+      this.$ = $;
+      this.url = url;
+    },
+    log: async function(message, data = {}) {
+      if (!this.enabled()) {
+        console.log("logs not enabled, stopping");
+        return;
+      }
+
+      const body = {
+        message,
+        data,
+        date: this.date,
+        context: this.context,
+      }
+
+      this.$.ajax({
+        url: this.url.build("paylike/index/Log"),
+        type: 'POST',
+        dataType: 'text',
+        data: body
+      });
+    },
+    enabled: function () {
+      return window.checkoutConfig.config.logsEnabled;
+    }
+  }
--- view/frontend/web/js/view/payment/method-renderer/paylikepaymentmethod-method.js
+++ view/frontend/web/js/view/payment/method-renderer/paylikepaymentmethod-method.js
@@ -11,14 +11,18 @@ define(
         'Magento_Checkout/js/view/payment/default',
         'Magento_Checkout/js/model/quote',
         'Magento_Checkout/js/action/place-order',
-        'Magento_Checkout/js/model/customer-email-validator'
+        'Magento_Checkout/js/model/customer-email-validator',
+        'Magento_Checkout/js/action/redirect-on-success',
+        'mage/url'
     ],
     function (ko,
               $,
               Component,
               quote,
               placeOrderAction,
-              customerEmailValidator) {
+              customerEmailValidator,
+              redirectOnSuccessAction,
+              url) {
         'use strict';
 
         return Component.extend({
@@ -74,16 +78,54 @@ define(
                 }
                 paylikeConfig.custom.customer.phoneNo = quote.billingAddress().telephone;
                 paylikeConfig.custom.customer.address = quote.billingAddress().street[0] + ", " + quote.billingAddress().city + ", " + quote.billingAddress().region + " " + quote.billingAddress().postcode + ", " + quote.billingAddress().countryId;
+
+                PaylikeLogger.setContext(paylikeConfig, $, url);
+
+                PaylikeLogger.log("Opening paylike popup");
+
                 paylike.popup(paylikeConfig, function (err, res) {
-                    if (err)
+                    if (err) {
+                        if(err === "closed") {
+                          PaylikeLogger.log("Paylike popup closed by user");
+                        }
+
                         return console.warn(err);
+                    }
 
                     if (res.transaction.id !== undefined && res.transaction.id !== "") {
                         self.payliketransactionid = res.transaction.id;
+                        PaylikeLogger.log("Paylike payment successfull. Transaction ID: " + res.transaction.id);
+                        /*
+                          In order to intercept the error of placeOrder request we need to monkey-patch
+                          the `addErrorMessage` function of the messageContainer:
+                           - first we duplicate the function on the same `messageContainer`, keeping the same `this`
+                           - next we override the function with a new one, were we log the error, and then we call the old function
+                        */
+                        self.messageContainer.oldAddErrorMessage = self.messageContainer.addErrorMessage;
+                        self.messageContainer.addErrorMessage = async function (messageObj) {
+                          await PaylikeLogger.log("Place order failed. Reason: " + messageObj.message);
+
+                          self.messageContainer.oldAddErrorMessage(message);
+                        }
+
+                        /*
+                          In order to log the placeOrder success, we need deactivate
+                          the redirect after order placed and call it manually, after
+                          we send the logs to the server
+                        */
+                        self.redirectAfterPlaceOrder = false;
+                        self.afterPlaceOrder = async function (args) {
+                          await PaylikeLogger.log("Order placed successfully");
+                          redirectOnSuccessAction.execute();
+                        }
+
+                        /* Everything is now setup, we can try placing the order */
                         self.placeOrder();
                     }
 
                     else {
+                        PaylikeLogger.log("No transaction id returned from paylike, order not placed");
+
                         return false;
                     }
                 });
