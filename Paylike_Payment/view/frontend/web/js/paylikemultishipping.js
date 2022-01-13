0ace111450f5f2f8ebba5864065eb7c4ae5d241f
LucianDex <remindfwd@gmail.com>
2022-01-13 10:35:28 +0200

    [feature] pay with multishipping

--- Paylike_Payment/view/frontend/web/js/paylikemultishipping.js
+++ Paylike_Payment/view/frontend/web/js/paylikemultishipping.js
@@ -1,9 +1,68 @@
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 /*browser:true*/
 /*global define*/
-///////////////////////// If it is necessary to have component code separate, paste it here ////////////////////
-///////////////////////// reference ////////////////////
-///////////////////////// /view/frontend/web/js/view/payment/method-renderer/paylikepaymentmethod-method.js ////////////////////
+define(
+    [
+        'jquery',
+        'Magento_Checkout/js/model/quote',
+        'Magento_Customer/js/model/customer',
+        'mage/url'
+    ],
+    function (
+            $,
+            quote,
+            customer,
+            url
+            ) {
+        'use strict';
+
+        return function displayPopup (callback) {
+
+            /** Initialize Paylike object. */
+            var paylike = Paylike({key: window.checkoutConfig.publicapikey});
+
+            var paylikeConfig = window.checkoutConfig.config;
+            var checkoutQuoteData = window.checkoutConfig.quoteData;
+            var checkoutCustomerData = window.checkoutConfig.customerData;
+
+            var multiplier = window.checkoutConfig.multiplier;
+            var grandTotal = parseFloat(checkoutQuoteData.grand_total);
+
+            paylikeConfig.amount.value = Math.round(grandTotal * multiplier);
+            window.paylikeminoramount = paylikeConfig.amount.value;
+
+
+            /** Change test key value from string 'test' with a boolean value. */
+            paylikeConfig.test = ('test' == paylikeConfig.test) ? (true) : (false);
+
+            quote.guestEmail = window.checkoutConfig.customerData.email;
+            /** Need to be logged in to perform checkout with multishipping. */
+            customer.setIsLoggedIn(window.checkoutConfig.isCustomerLoggedIn);
+
+            var customerAddresses = checkoutCustomerData.addresses;
+
+            /** Get billing address from customer addresses. */
+            var billingAddress = '';
+
+            for (var key in customerAddresses){
+                if (customerAddresses[key].default_billing) {
+                    var billingAddress = customerAddresses[key];
+                }
+            }
+
+            if (!customer.isLoggedIn) {
+                paylikeConfig.custom.customer.name = billingAddress.firstname + " " + billingAddress.lastname;
+                paylikeConfig.custom.customer.email = quote.guestEmail;
+            }
+
+            paylikeConfig.custom.customer.phoneNo = billingAddress.telephone;
+            paylikeConfig.custom.customer.address = billingAddress.inline;
+
+            PaylikeLogger.setContext(paylikeConfig, $, url);
+
+            PaylikeLogger.log("Opening paylike popup");
+
+            paylike.pay(paylikeConfig, callback);
+        };
+
+    }
+);
